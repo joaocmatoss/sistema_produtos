57672e3ef11d401ce4e37472315da8a2577c69ce
joaocmatoss <joao.cmatos@aluno.impacta.edu.br>
2026-05-27 17:10:46 -0300

    Update aaa.py
    Added functionality to search and remove products from the list.

--- aaa.py
+++ aaa.py
@@ -23,16 +23,34 @@ def produto_mais_barato(produtos):
 def media_precos(produtos):
     return sum(produtos.values()) / len(produtos)
 
+
 print('Relatório final')
 
 print(produtos)
 
-total = sum(produtos.values())
+
+# BUSCA
+nome_busca = input('Digite o nome do produto para buscar: ')
+print(produtos.get(nome_busca, 'Produto não encontrado'))
+
+
+# REMOVER
+nome_remover = input('Digite um produto para remover (ou ENTER para pular): ')
+
+if nome_remover != '':
+    if nome_remover in produtos:
+        del produtos[nome_remover]
+        print('Produto removido com sucesso!')
+    else:
+        print('Produto não encontrado')
+
+
+total = calcular_total(produtos)
 mais_caro = produto_mais_caro(produtos)
 mais_barato = produto_mais_barato(produtos)
 media = media_precos(produtos)
 
-print("Total:", total)
-print("Mais caro:", mais_caro)
-print("Mais barato:", mais_barato)
-print("Média:", media)
+print('Total:', total)
+print('Mais caro:', mais_caro)
+print('Mais barato:', mais_barato)
+print('Média:', media)
